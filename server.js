5a7a298e251c3decf52dc6640d1b2ac6d7fe8208
HusainNit <vaintime21@gmail.com>
2025-04-27 18:59:05 +0300

    done

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express');
-
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const loggin = require("morgan");
 const app = express();
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.send('The server is running');
+app.get("/", (req, res) => {
+  res.send("The server is running");
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
+  console.log(`Your secret is ${process.env.SECRET_PASSWORD}`);
 });
